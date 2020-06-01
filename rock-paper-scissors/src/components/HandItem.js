0d07c171b9deb6846796d9a9419eca4fa6b9fd87
Kaique <souzakaique2@gmail.com>
2020-06-01 14:07:46 -0300

    P: RPS Added & Styling three HandItem components

--- rock-paper-scissors/src/components/HandItem.js
+++ rock-paper-scissors/src/components/HandItem.js
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom";
 
 function HandItem(props){
     return(
-        <div className="circle d-flex align-items-center justify-content-center">
+        <div className="circle d-flex align-items-center justify-content-center" style={{borderColor: props.color}}>
+            <div className="circle-behind" style={{backgroundColor: props.behindcolor}}></div>
             <img src={props.image} />
         </div>
     );
--- rock-paper-scissors/src/components/ThreeHands.js
+++ rock-paper-scissors/src/components/ThreeHands.js
@@ -3,19 +3,23 @@ import ReactDOM from "react-dom";
 import HandItem from "./HandItem"
 
 import ImgHandPaper from "../img/icon-paper.svg"
+import ImgHandScissor from "../img/icon-scissors.svg"
+import ImgHandRock from "../img/icon-rock.svg"
 
 function ThreeHands(){
     return (
         <div className="threehand bordar">
             <div className="row m-0">
                 <div className="col-md-6 bordag p-0 d-flex justify-content-center">
-                    <HandItem image={ImgHandPaper}/>
+                    <HandItem image={ImgHandPaper} color="#5671f4" behindcolor="#2a45b9"/>
                 </div>
                 <div className="col-md-6 bordab p-0 d-flex justify-content-center">
-
+                    <HandItem image={ImgHandScissor} color="#eca821" behindcolor="#c56b1d"/>
                 </div>
             </div>
-
+            <div className="row m-0 d-flex justify-content-center">
+            <HandItem image={ImgHandRock} color="#dd415f" behindcolor="#991833" />
+            </div>
         </div>
     );
 }
