3ce410c332cff3d196ff4b0c39df55e7f1243cad
Kaique <souzakaique2@gmail.com>
2020-06-01 14:52:35 -0300

    P: RPS Fixed HandItems & Added more personalization to HandItems & background

--- rock-paper-scissors/src/components/ThreeHands.js
+++ rock-paper-scissors/src/components/ThreeHands.js
@@ -10,14 +10,14 @@ function ThreeHands(){
     return (
         <div className="threehand bordar">
             <div className="row m-0">
-                <div className="col-md-6 bordag p-0 d-flex justify-content-center">
+                <div className="col-md-5 bordag p-0 d-flex justify-content-end">
                     <HandItem image={ImgHandPaper} color="#5671f4" behindcolor="#2a45b9"/>
                 </div>
-                <div className="col-md-6 bordab p-0 d-flex justify-content-center">
+                <div className="col-md-5 offset-md-2 bordab p-0 d-flex justify-content-left">
                     <HandItem image={ImgHandScissor} color="#eca821" behindcolor="#c56b1d"/>
                 </div>
             </div>
-            <div className="row m-0 d-flex justify-content-center">
+            <div className="row m-0 d-flex justify-content-center align-items-center pt-4">
             <HandItem image={ImgHandRock} color="#dd415f" behindcolor="#991833" />
             </div>
         </div>
